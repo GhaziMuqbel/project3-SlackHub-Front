192f4ccb82b611de8d0f1982d0d5c930062e7d2d
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-06-13 11:21:15 +0300

    done with the singup form

---
+++ slackHub-frontEnd/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import Forms from "./components/Forms"
+
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div>
+        <Forms />
+      </div>
+    </>
+  )
+}
+
+export default App
