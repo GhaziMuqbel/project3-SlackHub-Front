e7ba60614489d5457d6b7c176fdef64240a8c7ae
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-06-13 14:12:29 +0300

    finish the page file

--- slackHub-frontEnd/src/App.jsx
+++ slackHub-frontEnd/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
 import Forms from "./components/Forms"
+import Register from "./pages/Register"
+import SignIn from "./pages/SignIn"
 
 import "./App.css"
+const [user, setUser] = useState(null)
 
 function App() {
   return (
     <>
       <div>
-        <Forms />
+        <SignIn setUser={setUser} />
       </div>
     </>
   )
