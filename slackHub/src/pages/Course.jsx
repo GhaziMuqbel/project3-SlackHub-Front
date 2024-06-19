e1b08cdd17c44dd309063bbf2a4765ec584103cf
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-06-19 12:25:56 +0300

    working on course

--- slackHub/src/pages/Course.jsx
+++ slackHub/src/pages/Course.jsx
@@ -2,11 +2,38 @@ import React from "react"
 import axios from "axios"
 import Assignment from "../components/Assignment"
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Course = () => {
-  const [assignment, setAssignment] = useState([])
+  const [assignments, setAssignments] = useState([])
+  const navigate = useNavigate()
 
-  return <div></div>
+  const getAssignments = async () => {
+    const response = await axios.get("http://localhost:3001/assignment")
+
+    setAssignments(response.data.results)
+  }
+
+  const handleAssignmentClick = (assignmentId) => {
+    navigate(`/course/details/${assignmentId}`)
+  }
+
+  useEffect(() => {
+    getAssignments()
+  }, [])
+
+  return (
+    <div>
+      {assignments.map((assignment) => (
+        <Assignment
+          key={assignment.id}
+          title={assignment.title}
+          description={assignment.description}
+          onClick={() => handleAssignmentClick(assignment.id)}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default Course
--- slackHub/src/pages/Home.jsx
+++ slackHub/src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import Course from "./Course"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
-  return <div>wwww</div>
+  const [courses, setCourses] = useState([])
+  const navigate = useNavigate()
+
+  const getCourse = async () => {
+    const response = await axios.get("http://localhost:3001/course")
+    setCourses(response.data.results)
+  }
+
+  const handleCourseClick = (courseId) => {
+    navigate(`/view/course/${courseId}`)
+  }
+
+  useEffect(() => {
+    getCourse()
+  }, [])
+
+  return (
+    <div>
+      <section>
+        {courses.map((course) => (
+          <Course
+            key={course.id}
+            title={course.title}
+            description={assignment.description}
+            onClick={() => handleCourseClick(course.id)}
+          />
+        ))}
+      </section>
+    </div>
+  )
 }
 
 export default Home
